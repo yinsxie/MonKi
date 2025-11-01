d5df5e59e86f35a0fb83caaa37dde2fdc1c48ffd
ZyVoir <whcs.william@gmail.com>
2025-11-01 22:54:57 +0700

    feat(relog): Garden CTA button integrated to Relog
    Gelooo klian, reusable ASF, 6 baris fitur dh jadi :")

--- MonKi/Pages/Garden/GardenViewModel.swift
+++ MonKi/Pages/Garden/GardenViewModel.swift
@@ -48,6 +48,8 @@ final class GardenViewModel: ObservableObject {
                 onApproveFieldTapped(log, context: context)
             case .done:
                 onDoneFieldTapped(log, context: context)
+            case .declined:
+                onDeclinedFieldTapped(log, context: context)
             default:
                 return
             }
@@ -102,4 +104,9 @@ private extension GardenViewModel {
             }
         }
     }
+    
+    func onDeclinedFieldTapped(_ log: MsLog, context: NavigationManager) {
+        context.goTo(.reLog(log: log))
+    }
+    
 }
